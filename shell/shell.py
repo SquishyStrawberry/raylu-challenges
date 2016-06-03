fdd2ef0fd0bf0562f6420b305c7254dece95d898
ColorfulBanana <therealrebuilder@gmail.com>
2016-06-03 22:51:14 +0200

    [shell] Use os.mknod instead of opening/closing a file object.

--- shell/shell.py
+++ shell/shell.py
@@ -15,10 +15,9 @@ def main():
         else:
             pid = os.fork()
             if pid == 0:
-                if len(args) >= 3 and args[-2] == ">":
+                if len(args) >= 2 and args[-2] == ">":
                     if not os.path.exists(args[-1]):
-                        with open(args[-1], "w") as _:
-                            pass
+                        os.mknod(args[-1])
                     os.dup2(os.open(args[-1], os.O_WRONLY), 1)
                     args = args[:-2]
                 os.execvp(command, [command, *args])
