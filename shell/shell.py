6988da53a2f565987d2684a8410d87992dc3ea6e
ColorfulBanana <therealrebuilder@gmail.com>
2016-06-03 22:36:41 +0200

    [shell] Builtins, just cd and pwd for now.

--- shell/shell.py
+++ shell/shell.py
@@ -9,12 +9,21 @@ def main():
         if pid == 0:
             if len(args) >= 3 and args[-2] == ">":
                 if not os.path.exists(args[-1]):
-                    with open(args[-1], "w") as f:
+                    with open(args[-1], "w") as _:
                         pass
                 os.dup2(os.open(args[-1], os.O_WRONLY), 1)
                 args = args[:-2]
-            os.execvp(command, [command, *args])
-        os.wait()
+            if command == "cd":
+                if len(args) < 2:
+                    os.chdir(os.environ["HOME"])
+                else:
+                    os.chdir(args[1])
+            elif command == "pwd":
+                print(os.getcwd())
+            else:
+                os.execvp(command, [command, *args])
+            os._exit(0)
+        os.waitpid(pid, 0)
 
 if __name__ == "__main__":
     main()
