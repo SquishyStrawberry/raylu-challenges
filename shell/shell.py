782d6dc157b4461464c5e5965f753f3a8bfa5aaf
ColorfulBanana <therealrebuilder@gmail.com>
2016-06-03 22:31:28 +0200

    [shell] I/O redirection

--- shell/shell.py
+++ shell/shell.py
@@ -7,6 +7,12 @@ def main():
         command, *args = input("> ").split()
         pid = os.fork()
         if pid == 0:
+            if len(args) >= 3 and args[-2] == ">":
+                if not os.path.exists(args[-1]):
+                    with open(args[-1], "w") as f:
+                        pass
+                os.dup2(os.open(args[-1], os.O_WRONLY), 1)
+                args = args[:-2]
             os.execvp(command, [command, *args])
         os.wait()
 
