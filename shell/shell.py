9ae6faa49069eab7b688a1dab61c95d6f35d24d8
ColorfulBanana <therealrebuilder@gmail.com>
2016-06-03 22:46:43 +0200

    [shell] Don't spawn a child process unless needed

--- shell/shell.py
+++ shell/shell.py
@@ -5,25 +5,26 @@ import os
 def main():
     while True:
         command, *args = input("> ").split()
-        pid = os.fork()
-        if pid == 0:
-            if len(args) >= 3 and args[-2] == ">":
-                if not os.path.exists(args[-1]):
-                    with open(args[-1], "w") as _:
-                        pass
-                os.dup2(os.open(args[-1], os.O_WRONLY), 1)
-                args = args[:-2]
-            if command == "cd":
-                if len(args) < 2:
-                    os.chdir(os.environ["HOME"])
-                else:
-                    os.chdir(args[1])
-            elif command == "pwd":
-                print(os.getcwd())
+        if command == "cd":
+            if len(args) < 2:
+                os.chdir(os.environ["HOME"])
             else:
+                os.chdir(args[1])
+        elif command == "pwd":
+            print(os.getcwd())
+        else:
+            pid = os.fork()
+            if pid == 0:
+                if len(args) >= 3 and args[-2] == ">":
+                    if not os.path.exists(args[-1]):
+                        with open(args[-1], "w") as _:
+                            pass
+                    os.dup2(os.open(args[-1], os.O_WRONLY), 1)
+                    args = args[:-2]
                 os.execvp(command, [command, *args])
-            os._exit(0)
-        os.waitpid(pid, 0)
+                raise RuntimeError("This should never happen!")
+            else:
+                os.waitpid(pid, 0)
 
 if __name__ == "__main__":
     main()
